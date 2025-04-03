810ea0aa9d4550215bdb6c61a1b6bc7edfc399a4
gram <git@orsinium.dev>
2025-04-03 11:36:44 +0200

    parse uint and float

--- parsers.go
+++ parsers.go
@@ -8,9 +8,17 @@ import (
 
 func Required(p parser) parser {
 	return func(raw string, ctx *context) error {
-		if ctx.env[raw] == "" {
+		if raw == "" {
 			return fmt.Errorf("required but not found")
 		}
+		return p(raw, ctx)
+	}
+}
+
+// Apply the function to the env var value before parsing it.
+func Map(p parser, f func(string) string) parser {
+	return func(raw string, ctx *context) error {
+		raw = f(raw)
 		return p(raw, ctx)
 	}
 }
@@ -55,17 +63,67 @@ func Int64[T ~int64](target *T) parser {
 	}
 }
 
+func Uint[T ~uint](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseUint(raw, 10, 0)
+		*target = T(val)
+		return err
+	}
+}
+
+func Uint8[T ~uint8](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseUint(raw, 10, 8)
+		*target = T(val)
+		return err
+	}
+}
+
+func Uint16[T ~uint16](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseUint(raw, 10, 16)
+		*target = T(val)
+		return err
+	}
+}
+
+func Uint32[T ~uint32](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseUint(raw, 10, 32)
+		*target = T(val)
+		return err
+	}
+}
+
+func Uint64[T ~uint64](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseUint(raw, 10, 64)
+		*target = T(val)
+		return err
+	}
+}
+
+func Float32[T ~uint64](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseFloat(raw, 32)
+		*target = T(val)
+		return err
+	}
+}
+
+func Float64[T ~uint64](target *T) parser {
+	return func(raw string, ctx *context) error {
+		val, err := strconv.ParseFloat(raw, 64)
+		*target = T(val)
+		return err
+	}
+}
+
 func Bool[T ~bool](target *T) parser {
 	return func(raw string, ctx *context) error {
-		switch raw {
-		case "true", "yes":
-			*target = true
-		case "false", "", "no":
-			*target = false
-		default:
-			return fmt.Errorf("must be true or false but got '%s'", raw)
-		}
-		return nil
+		val, err := strconv.ParseBool(raw)
+		*target = T(val)
+		return err
 	}
 }
 
